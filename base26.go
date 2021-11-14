8959de7c20583b16283fad63d18ed8a34425b691
line <line.xj.lin@gmail.com>
2021-11-14 10:36:37 +0000

    basic work

---
+++ base26.go
@@ -0,0 +1,33 @@
+package base26
+
+import (
+	"fmt"
+	bc "github.com/chtison/baseconverter"
+	"math/big"
+)
+
+type Base26 struct {
+	base26str string
+	dec       *big.Int
+}
+
+func NewBase26(in string) Base26 {
+	var b26 Base26
+	b26.base26str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	if d, e := bc.BaseToDecimal(in, b26.base26str); e != nil {
+		b26.dec = &big.Int{}
+	} else {
+		b26.dec = d
+	}
+	return b26
+}
+
+func (b *Base26) Add(i int64) Base26 {
+	b.dec = big.NewInt(0).Add(b.dec, big.NewInt(i))
+	return *b
+}
+
+func (b *Base26) String() string {
+	res, _ := bc.DecimalToBase(b.dec, b.base26str)
+	return res
+}
